99b246aa44badbf6c81dfa983bb21e944f195663
rushitarpe <rushitarpe44@gmail.com>
2025-02-03 20:13:34 +0530

    add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './component/shared/Header'
 import SignupFrom from './auth/SignupFrom'
+import Home from './pages/Home'
+import SigninFrom from './auth/SigninFrom'
+import About from './pages/About'
+import Tasks from './pages/Tasks'
+import Dashboard from './pages/Dashboard'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
@@ -12,7 +18,14 @@ const App = () => {
       <Routes>
 
        
-       <Route path="/signup" element={<SignupFrom />} />
+       <Route path="/sign-up" element={<SignupFrom />} />
+       <Route path="/sign-in" element={<SigninFrom />} />
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/task" element={<Tasks />} />
+       <Route path="/contact" element={<Contact />} />
+       <Route path="/dashboard" element={<Dashboard />} />
+
 
       </Routes>
       
