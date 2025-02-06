92f0fbd50ef2291b093656800c1702cd1b304eba
rushitarpe <rushitarpe44@gmail.com>
2025-02-06 13:48:31 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
-var figlet = require("figlet");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import figlet from 'figlet';
+dotenv.config();
 
 figlet("Welcome to Rushi's Website", function (err, data) {
     if (err) {
@@ -11,11 +13,13 @@ figlet("Welcome to Rushi's Website", function (err, data) {
     console.log(data);
   });
 
-
-
-
-
-
+mongoose.connect(process.env.MONGODB_URI).then( ()=> {
+    console.log('Connected to the database');
+    
+}).catch((err) => {
+    console.log('Connection failed');
+});
+const app = express();
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
