dec9c4a45254f2574c986eb5ba1a324becf950b5
Rushi <rushitarpe44@gmail.com>
2026-05-30 13:38:28 +0530

    Add fallback configuration variables for Appwrite and Firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_API,
+  apiKey:import.meta.env.VITE_FIREBASE_API || "AIzaSyB501qkE5cUH_6gkst7InRAGtFsJSt-KYs",
   authDomain: "prayas-ee040.firebaseapp.com",
   projectId: "prayas-ee040",
   storageBucket: "prayas-ee040.firebasestorage.app",
--- frontend/src/lib/appwrite/config.js
+++ frontend/src/lib/appwrite/config.js
@@ -1,9 +1,9 @@
 import { Client, Storage } from "appwrite"
 
 export const appwriteConfig = {
-  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID || "680492b60027defd6668",
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID || "680492f90013b4983440",
+  url: import.meta.env.VITE_APPWRITE_URL || "https://fra.cloud.appwrite.io/v1",
 }
 
 export const client = new Client()
